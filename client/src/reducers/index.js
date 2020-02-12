4e94d7cded5579a4205f4837ab63d8e398a1201f
Alexander <alexsantosantana@live.com>
2020-02-12 16:49:27 -0500

    Moving the front end to a seperate repository

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import AuthReducer from "./authenticationReducer";
-import InvoiceReducer from "./invoiceReducer";
-export default combineReducers({
-  user: AuthReducer,
-  invoice: InvoiceReducer
-});
